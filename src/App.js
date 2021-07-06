e035d123ba7c2fa4cd5049d2ab0e615096f3bf30
Daniel0Ferraz <50385288+daniel0ferraz@users.noreply.github.com>
2021-07-06 09:14:29 -0300

    finalizando projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import schema from './schema'
 import './App.css';
 
+
 function App() {
+
+  function onSubmit(values, actions) {
+    console.log('SUBMIT', values);
+  }
+
+  // function validate(values) {
+  //   const errors = {};
+  //   if (!values.name) {
+  //     errors.name = 'Nome é obrigatorio'
+  //   } if (!values.email) {
+  //     errors.email = 'Email é obrigatorio'
+  //   }
+  //   return errors;
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik
+        validationSchema={schema}
+        onSubmit={onSubmit}
+        // validate={validate}
+        validateOnMount
+        initialValues={{
+          name: '',
+          email: '',
+          password: '',
+        }}
+        render={({ values, errors, touched, isValid }) => (
+          <Form>
+            <div>
+              <label>Nome</label>
+              <Field name="name" type="text" />
+              <ErrorMessage name="name" />
+            </div>
+
+            <div>
+              <label>Email</label>
+              <Field name="email" type="email" />
+              <ErrorMessage name="email" />
+            </div>
+
+            <div>
+              <label>Senha</label>
+              <Field name="password" type="password" />
+              <ErrorMessage name="password" />
+            </div>
+
+            <button type="submit" disabled={!isValid}>Enviar</button>
+
+          </Form>
+        )}
+      />
+
     </div>
   );
 }
